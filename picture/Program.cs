8c44f666a6f519db1dccb6f7eac6370444350d9a
GermanFA <79627686+GermanFA@users.noreply.github.com>
2021-02-26 05:13:30 +0300

    Добавьте файлы проекта.

---
+++ picture/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace picture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picture = 52;
+            int zone = 3;
+            int row;
+
+            row = picture / zone;
+            picture %= zone;
+            Console.WriteLine("Полных рядов будет - " + row + ",остаток - " + picture);
+        }
+    }
+}
